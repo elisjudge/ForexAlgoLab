0bab16a8293b445b27d9ae277c8ac3c9262b7bf6
elisjudge <101382345+elisjudge@users.noreply.github.com>
2023-12-21 00:23:31 +0900

    server test

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+# Configure CORS to allow requests from localhost:3000
+CORS(app, resources={r"/test": {"origins": "http://localhost:3000"}})
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/test')
+def test():
+    return {'message': 'Connection successful'}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect} from 'react';
+
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/test')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Fetch error:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Message from Flask: {message}</p>
     </div>
   );
 }
-
 export default App;
